6c371534bced4e7769c0065eba14637da5be94f0
Thurloat <thurloat@gmail.com>
2014-03-01 22:26:31 -0400

    use the right version path

--- bricks/common/config.py
+++ bricks/common/config.py
@@ -5,7 +5,7 @@ from oslo.config import cfg
 from bricks.common import paths
 from bricks.openstack.common.db.sqlalchemy import session as db_session
 from bricks.openstack.common import rpc
-from bricks import version
+from bricks import __version__ as bricks_version
 
 _DEFAULT_SQL_CONNECTION = 'sqlite:///' + paths.state_path_def('$sqlite_db')
 
@@ -16,5 +16,5 @@ def parse_args(argv, default_config_files=None):
     rpc.set_defaults(control_exchange='bricks')
     cfg.CONF(argv[1:],
              project='bricks',
-             version=version.version_string(),
+             version=bricks_version,
              default_config_files=default_config_files)
