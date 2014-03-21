0bde66a0fb2c2f1cb230cb3339ce5f9eb04e1cbe
Thurloat <thurloat@gmail.com>
2014-03-21 11:40:12 -0300

    add stubbed out utils

---
+++ bricks/mortar/utils.py
@@ -0,0 +1,10 @@
+def do_execute(req_context, execution_list):
+    """Executes a list of arbitrary shit from the conductor, it will
+    receive all tasks, so it needs to determine which hosts locally it can
+    send commands to, and do so.
+
+    :param req_context:
+    :param execution_list ([objects.MortarTask, ]): A list of tasks to do
+    work on.
+    """
+    pass
