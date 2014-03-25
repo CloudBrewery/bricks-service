54ea0f44bf2577831e3b9aa7ac817cadebfc78b4
swat30 <jacobgodin@gmail.com>
2014-03-24 22:54:44 -0300

    Bye-bye socket receive

--- bricks/mortar/utils.py
+++ bricks/mortar/utils.py
@@ -2,8 +2,6 @@ import json
 import socket
 import struct
 
-from bricks.common import exception
-
 FORMAT = '!I'
 FORMAT_LEN = 4
 SOCKET_TIMEOUT = 10
@@ -19,18 +17,6 @@ def socket_send(sock, message):
     sock.sendall(''.join([header, data]).join('\n'))
 
 
-def socket_receive(sock):
-    try:
-        header = sock.recv(FORMAT_LEN)
-        length = struct.unpack(FORMAT, header)[0]
-        data = sock.recv(length)
-        message = json.loads(data)
-        return message
-    except Exception as e:
-        sock.close()
-        raise e
-
-
 def do_execute(req_context, execution_list):
     """Executes a list of arbitrary shit from the conductor, it will
     receive all tasks, so it needs to determine which hosts locally it can
@@ -40,8 +26,6 @@ def do_execute(req_context, execution_list):
     :param execution_list ([objects.MortarTask, ]): A list of tasks to do
     work on.
     """
-    results = []
-
     for task in execution_list:
         ##TODO: CHANGE THIS BACK
         #socket_file = "/tmp/mortar/%s.socket" % task.instance_id
@@ -51,21 +35,7 @@ def do_execute(req_context, execution_list):
 
         try:
             sock.connect(socket_file)
-        except socket.error as e:
-            results.append((task.instance_id, False,
-                            "Could not connect to socket. (%s)" % e))
-            sock.close()
+        except socket.error:
             continue
 
         socket_send(sock, task.raw_command)
-
-        try:
-            receive_message = socket_receive(sock)
-        except socket.error as e:
-            results.append((task.instance_id, False,
-                            e.message))
-            continue
-
-        results.append((task.instance_id, True, receive_message))
-
-    return results
