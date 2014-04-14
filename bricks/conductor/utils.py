f39aff7ff7a1f68c07600f46cc0b3bb49a84518c
Thurloat <thurloat@gmail.com>
2014-04-14 11:12:32 -0300

    Attempt to get all servers for all tenants

--- bricks/conductor/utils.py
+++ bricks/conductor/utils.py
@@ -126,8 +126,9 @@ def deleted_instances_cleanup_action(req_context):
     # get all nova instances
     novaclient = opencrack.build_nova_client(req_context)
     novaclient.authenticate()
-    servers = novaclient.servers.list()
+    servers = novaclient.servers.list(seach_opts={'all_tenants': 1})
     server_uuids = [server.id for server in servers]
+
     LOG.debug("Have %s instances" % len(server_uuids))
 
     # generate a list of bricks whose instance_ids don't show up in the
